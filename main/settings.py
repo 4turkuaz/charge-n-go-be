40341828dfc9731437592d99c4a24de6013816d1
Berkay Ugur Senocak <berkayacademic@gmail.com>
2022-11-20 02:49:08 +0100

    updated cors

--- main/settings.py
+++ main/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -58,8 +58,8 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+CORS_ALLOW_HEADERS = "*"
 CORS_ORIGIN_ALLOW_ALL = True
-
 
 ROOT_URLCONF = "main.urls"
 
